9ab5e7703a21d553bcd5ecf3d38f9d72a244900c
Sajjad1276 <sajjad.1390125@gmail.com>
2026-09-13 08:36:12 +0330

    feat: register Rubika bot interface package

---
+++ src/ad_network/bots/__init__.py
@@ -0,0 +1,5 @@
+from .admin_bot import build_admin_bot
+from .owner_bot import build_owner_bot
+from .user_bot import build_user_bot
+
+__all__ = ["build_admin_bot", "build_owner_bot", "build_user_bot"]
